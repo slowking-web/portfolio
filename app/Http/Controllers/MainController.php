43dcbd2ab3cabc596c59dc0b90ff0ba4d055aed9
Yohei Fukuda <yfkmmt@outlook.jp>
2021-09-04 17:50:30 +0900

    Create display function of "Skill"

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Item;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
@@ -16,10 +18,22 @@ class MainController extends Controller
     
     public function index()
     {
+        // ABOUT MEの表示ファイルを取得
         $aboutme = Storage::disk('public')->get($this->fname);
         
+        // MY SKILLSの表示データを取得
+        $items = Item::where('sub_id', 1)->get();
+        
+        $skills = DB::table('skills')
+        ->leftJoin('items', 'skills.items_id', '=', 'items.sort_id')
+        ->where('items.sub_id', '=', 1)
+        ->select('skills.id', 'skills.items_id', 'skills.name', 'skills.skill')
+        ->get();
+        
         $data = [
-            'aboutme' => $aboutme
+            'aboutme' => $aboutme,
+            'items' => $items,
+            'skills' => $skills
         ];
         
         return view('pf.index', $data);
--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -12,5 +12,24 @@
     </section>
     <section class="skills">
     	<h2 class="heading">MY SKILLS</h2>
+    	<div class="skills-wrapper">
+    	<table align="center">
+    		@foreach ($items as $item)
+    		<thead>
+			<tr><th colspan="2" class="skill-title">{{$item->name}}</th></tr>
+			</thead>
+    			@foreach ($skills as $skill)
+    			@if ($item->sort_id == $skill->items_id)
+    			<tbody>
+        		<tr>
+        			<td><p class="skill-text">{{$skill->name}}</p></td>
+        			<td><p class="skill-text">{{$skill->skill}}</p></td>
+        		</tr>
+        		</tbody>
+        		@endif
+        		@endforeach
+    		@endforeach
+    	</table>
+    	</div>
     </section>
 </div>
